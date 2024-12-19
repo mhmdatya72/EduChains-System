28a05df7e8bd20576deba1ab4d2ddaad6fd2f3f8
mhmdatya72 <alahly723@gmail.com>
2024-12-19 19:49:32 +0200

    Modifying the footer and the Contact Us page, and converting all texts to Educhains.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(SectionsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+        $this->call(SectionsTableSeeder::class);
     }
 }
